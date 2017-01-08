68a1db24f5759bf7ea7583fe56ad3f096c5db5ed
ElDainosor <eldainosor@gmail.com>
2017-01-08 13:12:41 -0300

    rebrand: change to

--- releasetools.py
+++ releasetools.py
@@ -39,7 +39,7 @@ def AddAssertion(info):
   info.script.AppendExtra('ui_print("fastboot oem config fsg-id ringplus if you are a RingPlus user");')
   info.script.AppendExtra('ui_print("fastboot oem config fsg-id sprint   if you are a Sprint user or not listed");')
   info.script.AppendExtra('ui_print("fastboot oem config fsg-id virgin   if you are a Virgin Mobile user");')
-  info.script.AppendExtra('ui_print("and then flash LineageOS again");')
+  info.script.AppendExtra('ui_print("and then flash Resurrection Remix again");')
   info.script.AppendExtra('abort("**********");')
   info.script.AppendExtra('endif;')
 
